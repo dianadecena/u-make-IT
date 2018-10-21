2f0373e99c4fec903d79e6db5a2de3ac5c57241a
dianadecena <dianadecena78@gmail.com>
2018-10-21 11:26:39 -0400

    'routing'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PizzasComponent } from './pizzas/pizzas.component';
-import { BebidasComponent } from './bebidas/bebidas.component';
-import { OrdenComponent } from './orden/orden.component';
-import { ComprasComponent } from './compras/compras.component';
+import { LoginAndRegisterComponent } from './login-and-register/login-and-register.component';
 import { HomeComponent } from './home/home.component';
-import { BarraSuperiorComponent } from './barra-superior/barra-superior.component';
+import { BarraSuperiorComponent } from './barra-superior/barra-superior.component'; 
+import { PizzasComponent } from './pizzas/pizzas.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { OrdenComponent } from './orden/orden.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { BebidasComponent } from './bebidas/bebidas.component';
+import { PostresComponent } from './postres/postres.component';
+
+const appRoutes:Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'pizzas',
+    component: PizzasComponent
+  },
+  {
+    path: 'orden',
+    component: OrdenComponent
+  },
+    {
+    path: 'change-password',
+    component: ChangePasswordComponent
+  },
+  {
+    path: 'bebidas',
+    component: BebidasComponent
+  },
+    {
+    path: 'postres',
+    component: PostresComponent
+  },
+  {
+    path: '',
+    component: LoginAndRegisterComponent
+  }
+  ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzasComponent,
-    BebidasComponent,
-    OrdenComponent,
-    ComprasComponent,
+    LoginAndRegisterComponent,
     HomeComponent,
-    BarraSuperiorComponent
-  ],
+    BarraSuperiorComponent,
+    PrincipalComponent,
+    PizzasComponent,
+    OrdenComponent,
+    ChangePasswordComponent,
+    BebidasComponent,
+    PostresComponent],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    RouterModule.forRoot(appRoutes),
+    BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
