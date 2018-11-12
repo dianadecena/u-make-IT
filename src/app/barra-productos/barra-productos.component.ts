c512aa8ba63236d8aa9582c8244691eaa3845908
danielmesa20 <43939549+danielmesa20@users.noreply.github.com>
2018-11-11 21:30:22 -0400

    Update barra-productos.component.ts

--- src/app/barra-productos/barra-productos.component.ts
+++ src/app/barra-productos/barra-productos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-barra-productos',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarraProductosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auten: AuthService, private router: Router) { }
 
   ngOnInit() {
+    
+  }
+  
+  salir(){
+    this.auten.doLogout();
   }
 
 }
