8c6d6864e39b6f4adf39c05d478254e6a09cfc83
danielmesa20 <43939549+danielmesa20@users.noreply.github.com>
2018-11-12 23:09:27 -0400

    upgrade agregar postre (.ts)

--- src/app/agregar-postre/agregar-postre.component.ts
+++ src/app/agregar-postre/agregar-postre.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { PostresService } from "../services/postres.service";
+import { Producto } from "../models/producto";
 
 @Component({
   selector: 'app-agregar-postre',
   templateUrl: './agregar-postre.component.html',
-  styleUrls: ['./agregar-postre.component.scss']
+  styleUrls: ['./agregar-postre.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AgregarPostreComponent implements OnInit {
 
-  constructor() { }
+  postre: Producto = {
+    nombre:'',
+    descripcion:'',
+    precio:'',
+    imagen:'',
+    disponible:'',
+  };
+
+  mensaje : string = '';
+
+  constructor(private BebidasService: PostresService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    if ((this.postre.disponible == 'si'  || this.postre.disponible == 'no') && this.postre.precio != ""  &&
+     this.postre.imagen.length > 0 && this.postre.nombre.length > 0 && this.postre.descripcion.length > 0 ) {
+      this.BebidasService.addPostre(this.postre);
+      this.postre.nombre = '';
+      this.postre.descripcion = '';
+      this.postre.precio = '';
+      this.postre.imagen = '';
+      this.postre.disponible = '';
+      this.mensaje = "La operacion se ha realizado exitosamente";
+      console.log("bien");
+    }else{
+      this.mensaje = "Hay algun error en los campos ingresados o falta alguno por rellenar";
+      console.log("error");
+    }
   }
 
 }
