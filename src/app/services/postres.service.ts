d71c0b10bb9afffa0cefd845c2551e8a0e2a29ee
danielmesa20 <43939549+danielmesa20@users.noreply.github.com>
2018-11-12 22:54:20 -0400

    cambios servicio postres

--- src/app/services/postres.service.ts
+++ src/app/services/postres.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Producto } from '../models/producto';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
+
 export class PostresService {
 
-  constructor() { }
+  productsCollection: AngularFirestoreCollection<Producto>;
+  postres:Observable<Producto[]>;
+  postreDoc:AngularFirestoreDocument<Producto>;
+
+  result:any;
+
+  constructor(public angularFirestore:AngularFirestore) { 
+    this.productsCollection = this.angularFirestore.collection('postres');
+    this.postres = this.productsCollection.snapshotChanges().pipe(map(changes =>{
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Producto;
+        data.id = a.payload.doc.id;
+        return data;
+      });
+    }));
+   }
+
+   getPostres() {
+    return this.angularFirestore.collection('postres').snapshotChanges().pipe(map(changes =>{
+       return changes.map(a =>{
+         const data = a.payload.doc.data() as Producto;
+         data.id = a.payload.doc.id;
+         return data;
+       });
+     }));
+   }
+
+   addPostre(postre:Producto){
+     this.productsCollection.add(postre);
+   }
+
+  updatePostre(postre: Producto) {
+    this.postreDoc = this.angularFirestore.doc(`postres/${postre.id}`);
+    this.postreDoc.update(postre);
+  }
+
+  deletePostre(postre: Producto) {
+    this.postreDoc = this.angularFirestore.doc(`postres/${postre.id}`);
+    this.postreDoc.delete();
+  }
 }
